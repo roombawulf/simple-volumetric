e93e4ee67a31277d54fbc383b019a6a5d871714b
roombawulf <prydax997@gmail.com>
2024-05-18 14:43:30 +0100

    remove text, collapse menubar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
+import { Leva } from "leva";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Clouds from "./Clouds";
 
 function App() {
     return (
-        <Canvas dpr={1.0}>
-            <Clouds />
-            <OrbitControls />
+        <>
+            <Leva collapsed={true} />
+            <Canvas dpr={1.0}>
+                <Clouds />
+                <OrbitControls />
 
-            <mesh position={[0, 0, -5]} scale={0.5}>
-                <sphereGeometry />
-                <meshStandardMaterial color="aliceblue" />
-            </mesh>
+                <mesh position={[0, 0, -5]} scale={0.5}>
+                    <sphereGeometry />
+                    <meshStandardMaterial color="aliceblue" />
+                </mesh>
 
-            <ambientLight intensity={0.5} />
-            <spotLight
-                position={[1000, 1000, 1000]}
-                angle={0.15}
-                penumbra={1}
-            />
-            <pointLight position={[-1000, -1000, -1000]} />
-        </Canvas>
+                <ambientLight intensity={0.5} />
+                <spotLight
+                    position={[1000, 1000, 1000]}
+                    angle={0.15}
+                    penumbra={1}
+                />
+                <pointLight position={[-1000, -1000, -1000]} />
+            </Canvas>
+        </>
     );
 }
 
